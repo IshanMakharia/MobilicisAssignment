bdefd276c6f876a93f99aa12c77ac46a8f794509
Ishan Makharia <ishanmakharia@Ishans-MacBook-Air.local>
2023-04-25 16:22:10 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose  = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dataRoute = require("./routes/data");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(
+    ()=>{ console.log("Connected to MongoDB") }
+);
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/data", dataRoute);
+
+app.listen(8000, ()=>{
+    console.log(("Backend server is running!"))
+})
